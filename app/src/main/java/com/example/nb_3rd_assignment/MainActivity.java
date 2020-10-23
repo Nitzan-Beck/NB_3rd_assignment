25d7f2df710151f3d3a3d256ea3eab044291dbd5
Nitzan-Beck <s1494@motzkine.ort.org.il>
2020-10-23 12:48:19 +0300

    2nd update

--- app/src/main/java/com/example/nb_3rd_assignment/MainActivity.java
+++ app/src/main/java/com/example/nb_3rd_assignment/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -19,5 +20,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void proccessGame(View view) {
+        Button chooseBtn=(Button) view;
+        chooseBtn.setText("X");
+        chooseBtn.setBackgroundResource(R.color.XOColor);
+        chooseBtn.setEnabled(false);
+        int cellNo=0;
+        if (chooseBtn.getId()==R.id.btn1) {
+            cellNo = 1;
+        }
+        if (chooseBtn.getId()==R.id.btn2) {
+            cellNo = 2;
+        }
+        if (chooseBtn.getId()==R.id.btn3) {
+            cellNo = 3;
+        }
+        if (chooseBtn.getId()==R.id.btn4) {
+            cellNo = 4;
+        }
+        if (chooseBtn.getId()==R.id.btn5) {
+            cellNo = 5;
+        }
+        if (chooseBtn.getId()==R.id.btn6) {
+            cellNo = 6;
+        }
+        if (chooseBtn.getId()==R.id.btn7) {
+            cellNo = 7;
+        }
+        if (chooseBtn.getId()==R.id.btn8) {
+            cellNo = 8;
+        }
+        if (chooseBtn.getId()==R.id.btn9) {
+            cellNo = 9;
+        }
     }
 }
