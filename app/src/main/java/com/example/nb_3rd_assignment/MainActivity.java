1d8f9fde12b053dc21581e081a31646fd313a01d
Nitzan-Beck <s1494@motzkine.ort.org.il>
2020-10-23 12:33:49 +0300

    first update

--- app/src/main/java/com/example/nb_3rd_assignment/MainActivity.java
+++ app/src/main/java/com/example/nb_3rd_assignment/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.nb_3rd_assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Integer>player1=new ArrayList<>();
+        ArrayList<Integer>player2=new ArrayList<>();
+    }
+
+    public void proccessGame(View view) {
     }
 }
